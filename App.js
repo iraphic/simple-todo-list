e7dac8da5afbf596be42be77934f66bd0ca821b9
Dody Prasetya <dody.prasetya@btpn.com>
2019-10-11 16:48:42 +0700

    Implement apollo client and log the response

--- App.js
+++ App.js
@@ -1,12 +1,48 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import ApolloClient, { gql } from 'apollo-boost';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+const client = new ApolloClient({
+  uri: 'https://api.graph.cool/simple/v1/cjyiicfwm2rzb0113sx7eavqw'
+});
+
+const todoListQuery = gql`
+  query GetTodoList {
+    allTodoes {
+      id
+      title
+      content
+      isFinished
+    }
+  }
+`;
+
+class App extends Component {
+
+  componentDidMount() {
+    this.loadData();
+  }
+
+  loadData() {
+    client.query({
+      query: todoListQuery,
+      fetchPolicy: 'network-only'
+    })
+    .then(result => {
+      console.log(result.data);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+  }
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Hello World!</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
@@ -17,3 +53,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
