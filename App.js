9597857fdc250fe1d9f1bc9734d21eb951189727
Dody Prasetya <dody.prasetya@btpn.com>
2019-10-11 17:35:39 +0700

    Display data using FlatList

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
 import ApolloClient, { gql } from 'apollo-boost';
 
 const client = new ApolloClient({
@@ -48,11 +48,26 @@ class App extends Component {
     });
   }
 
+  renderItem = (item) => {
+    return (
+      <View style={{ flexDirection: 'column', flex: 1, marginHorizontal: 20, marginVertical: 10 }}>
+
+        <Text style={{ fontSize: 15, fontWeight: '500' }}>{item.title}</Text>
+        <Text>{item.content}</Text>
+
+      </View>
+    );
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.header}>Todo List</Text>
-        <Text>Hello World!</Text>
+        <FlatList
+          data={this.state.todoList}
+          renderItem={({ item }) => this.renderItem(item)}
+          keyExtractor={({ item }, index) => index.toString()}
+        />
       </View>
     );
   }
